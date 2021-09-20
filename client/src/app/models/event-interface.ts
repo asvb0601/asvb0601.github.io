28672b028cc14d6d9afd76a44d63827075cc802a
asvb0601 <andres.vb0601@gmail.com>
2021-09-20 15:49:28 -0500

    Coreeccion conexion a base de datos

---
+++ client/src/app/models/event-interface.ts
@@ -0,0 +1,10 @@
+export interface EventInterface{
+    titulo?: string;
+    descripcion?: string;
+    confirmados?:number;
+    vacantes?:number;
+    encuentro?: string;
+    finalizacion?: string;
+    direccion?: string;
+    id?: string;
+}
--- client/src/app/services/data-api.service.ts
+++ client/src/app/services/data-api.service.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
+import {map} from 'rxjs/operators';
+
+import { EventInterface } from '../models/event-interface';
+import { AuthService } from './auth.service';
 @Injectable({
   providedIn: 'root'
 })
 export class DataApiService {
-  evento!: Observable<any>;
-  eventos!: Observable<any>;
-  constructor(private http: HttpClient) { }
   
+  constructor(private http: HttpClient, private authService:AuthService) { }
+  evento: Observable<any>;
+  eventos: Observable<any>;
+  headers : HttpHeaders =new HttpHeaders({
+    "Content-Type": "aplication/json",
+    Authorization: this.authService.getToken()
+  });
+
   getAllEvents(){
     const url_api = 'http://localhost:3000/api/enentos';
     return this.http.get(url_api);
@@ -21,11 +30,25 @@ export class DataApiService {
 
   }
 
-  saveEvent(evento){
+  saveEvent(evento: EventInterface){
     let token;
     const url_api =  `http://localhost:3000/api/productos/${token}`;
-    return this.http.post(url_api evento,{})
-    .pipe(map(data) => data));
+    return this.http.post<EventInterface>(url_api, evento,{headers : this.headers})
+    .pipe(map(data => data));
+  }
+
+  updateEvent(evento){
+    let token = this.authService.getToken();
+    const url_api =  `http://localhost:3000/api/productos/${token}`;
+    return this.http.put<EventInterface>(url_api, this.evento,{headers : this.headers})
+    .pipe(map(data => data));
+  }
+
+  deleteEvent(id: string){
+    let token = this.authService.getToken();
+    const url_api =  `http://localhost:3000/api/productos/${id}`;
+    return this.http.delete(url_api, {headers : this.headers})
+    .pipe(map(data => data));
   }
 
 }
